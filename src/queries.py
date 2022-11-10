3db9c0498856bd7efd3212d743ac219bdc3e938d
Shekha32 <alexshekha32@gmail.com>
2022-11-10 23:40:59 +0300

    Add print table

--- src/queries.py
+++ src/queries.py
@@ -2,8 +2,8 @@
 #Execution of various queries to the database (PostgreSQL)
 
 #system libraries
-import pprint
 import psycopg2
+from pprint import pprint
 
 
 #drop table
@@ -13,18 +13,25 @@ def __droptable ( db, tablename ) -> None:
         db.query ( query )
 
 
-#create table (SQL)
+#create table from data (SQL)
 def __createtable ( db ) -> None:
 
         query = open ( './data/employee.sql', 'r' ).read()
         db.query ( query )
 
 
-#create table (CSV->SQL)
+#create table from data (CSV->SQL)                      #TODO
 def __createtablecsv ( db, tablename ) -> None:
 
         pass
         #db.query ( query )
+
+
+#print table
+def __printtable ( db, tablename ) -> None:
+
+        query = f"SELECT * FROM {tablename};"
+        pprint ( db.query ( query, selection=True ), width=200 )
 
 
 #various queries
@@ -36,6 +43,7 @@ def queries ( db ):
                 __droptable ( db, tablename )           #drop table if exists
                 __createtable ( db )                    #create table from data (SQL)
                 #__createtablecsv ( db, tablename )     #create table from data (CSV->SQL)
+                __printtable ( db, tablename )          #print table
 
         except ( Exception, psycopg2.DatabaseError ) as error:
                 exit ( error )
