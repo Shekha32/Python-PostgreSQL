42d34515a893977600cff25a69365620e411390a
Shekha32 <alexshekha32@gmail.com>
2022-11-14 22:51:21 +0300

    __printcolumns

--- src/queries.py
+++ src/queries.py
@@ -60,10 +60,17 @@ def __printtable ( db, tablename ) -> None:
         pprint ( db.query ( query, selection=True ), width=200 )
 
 
-#print table
+#print one column
 def __printcolumn ( db, tablename, column ) -> None:
 
         query = f"SELECT {column} FROM {tablename};"
+        pprint ( db.query ( query, selection=True ), width=200 )
+
+
+#print several columns
+def __printcolumns ( db, tablename, columns ) -> None:
+
+        query = f"SELECT {','.join ( columns )} FROM {tablename};"
         pprint ( db.query ( query, selection=True ), width=200 )
 
 
@@ -73,12 +80,14 @@ def queries ( db ):
         tablename = 'employee'
 
         try:
-                __droptable ( db, tablename )                           #drop table if exists
-                __createtable ( db )                                    #create table from data (SQL)
-                __droptable ( db, tablename )                           #drop table if exists
-                __createtablecsv ( db, tablename )                      #create table from data (CSV->SQL)
-                #__printtable ( db, tablename )                         #print table
-                __printcolumn ( db, tablename, column='email' )         #print one column
+                __droptable ( db, tablename )                                                           #drop table if exists
+                __createtable ( db )                                                                    #create table from data (SQL)
+                __droptable ( db, tablename )                                                           #drop table if exists
+                __createtablecsv ( db, tablename )                                                      #create table from data (CSV->SQL)
+                #__printtable ( db, tablename )                                                         #print table
+                __printcolumn ( db, tablename, column='email' )                                         #print one column
+                __printcolumns ( db, tablename, columns=['first_name', 'last_name', 'title'] )          #print several columns
+
 
         except ( Exception, psycopg2.DatabaseError ) as error:
                 exit ( error )
