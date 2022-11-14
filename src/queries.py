e51682d66893d4cd3cde099e754cc574bf49134b
Shekha32 <alexshekha32@gmail.com>
2022-11-14 23:54:59 +0300

    __limit

--- src/queries.py
+++ src/queries.py
@@ -101,6 +101,14 @@ def __whereand ( db, tablename, columns ) -> None:
         pprint ( db.query ( query, selection=True ), width=200 )
 
 
+#limit
+def __limit ( db, tablename, limit ) -> None:
+
+        query = f"SELECT * FROM {tablename} LIMIT {limit};"
+        print ( '\n', query )
+        pprint ( db.query ( query, selection=True ), width=200 )
+
+
 #various queries
 def queries ( db ):
 
@@ -111,12 +119,13 @@ def queries ( db ):
                 __createtable ( db )                                                                    #create table from data (SQL)
                 __droptable ( db, tablename )                                                           #drop table if exists
                 __createtablecsv ( db, tablename )                                                      #create table from data (CSV->SQL)
-                #__printtable ( db, tablename )                                                         #print table
+                __printtable ( db, tablename )                                                          #print table
                 __printcolumn ( db, tablename, column='email' )                                         #print one column
                 __printcolumns ( db, tablename, columns=['first_name', 'last_name', 'title'] )          #print several columns
                 __orderby ( db, tablename, orderby='city', sort='DESC' )                                #print table order by {field} (ASC/DESC)
                 __distinct ( db, tablename, column='country', sort='ASC' )                              #distinct rows in column
                 __whereand ( db, tablename, columns=['gender', 'country'] )                             #where + and
+                __limit  ( db, tablename, limit=12 )                                                    #limit
 
         except ( Exception, psycopg2.DatabaseError ) as error:
                 exit ( error )
